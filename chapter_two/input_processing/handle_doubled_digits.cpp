16e886606f01f6edd8afe853d6281fb2b7eb3df5
Nafio Nabi <nafionabi.work.acc@gmail.com>
2024-05-27 12:05:28 +0600

    Handle double digit.

--- chapter_two/input_processing/handle_doubled_digits.cpp
+++ chapter_two/input_processing/handle_doubled_digits.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-using std::cin;
 using std::cout;
+using std::cin;
 
 int doubleDigitValue(int digit) {
     int doubledDigit = digit * 2;
@@ -12,11 +12,11 @@ int doubleDigitValue(int digit) {
 
 int main() {
     int digit;
-    cout << "Enter a single digit number, 0-9: ";
+    cout << "Enter a single digit number, 0-9: "; 
     cin >> digit;
-    int doubledDigit = digit * 2;
+    int doubledDigit = digit * 2; 
     int sum;
-    if (doubledDigit >= 10) sum = 1 + doubledDigit % 10;
+    if (doubledDigit >= 10) sum = 1 + doubledDigit % 10; 
     else sum = doubledDigit;
     cout << "Sum of digits in doubled number: " << sum << "\n";
 }
