8a7d9b21b97755a2278bba11e4b15ba5218b4c46
Nafio Nabi <nafionabi.work.acc@gmail.com>
2024-06-01 12:45:32 +0600

    Read number with three or four digits.

--- chapter_two/tracking_state/read_num_with_three_or_four_digits.cpp
+++ chapter_two/tracking_state/read_num_with_three_or_four_digits.cpp
@@ -3,13 +3,15 @@ using std::cin;
 using std::cout;
 
 int main() {
-    cout << "Enter a number with as many digits as you like: "; 
-    char digitChar = cin.get();
-    int number = (digitChar - '0');
-    digitChar = cin.get();
-    while (digitChar != 10) {
-        number = number * 10 + (digitChar - '0');
+    char digitChar; 
+    do {
         digitChar = cin.get();
-    }
-   cout << "Numbered entered: " << number << "\n";
+        int number = (digitChar - '0');
+        digitChar = cin.get();
+        while ((digitChar != 10) && (digitChar != ',')) {
+            number = number * 10 + (digitChar - '0');
+            digitChar = cin.get();
+       }
+        cout << "Numbered entered: " << number << "\n"; } 
+        while (digitChar != 10);
 }
