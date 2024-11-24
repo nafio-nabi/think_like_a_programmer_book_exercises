0def3729d66dcd5b27be15c50bee3eba79a09ddf
Nafio Nabi <nafionabi@Nafios-MacBook-Air.local>
2024-11-24 14:19:06 +0600

    a sideways triangle

---
+++ chapter_2/output_patterns/a_sideways_triangle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main() {
+    for (int row = 1; row <= 7; row++) {
+        for (int hashNum = 1; hashNum <= 4 - abs(4 - row); hashNum++) {
+            cout << "#";
+        }
+        cout << "\n";
+    }
+}
