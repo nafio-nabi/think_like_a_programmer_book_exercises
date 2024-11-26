4e7f95831e0175bacaa04d55dcda0c3f57a0bfa3
Nafio Nabi <nafionabi@Nafios-MacBook-Air.local>
2024-11-26 15:35:34 +0600

    chapter 2, exercise 2-1.

---
+++ chapter_2/exercises/ex2-1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+using std::cin;
+using std::cout;
+using std::setw;
+
+int main() {
+    int counter = 0;
+
+    for (int row = 1; row <= 4; row++) {
+        for (int spaceCounter = 1; spaceCounter < row; spaceCounter++) {
+            cout << " ";
+        }
+
+        for (int hashNum = 1; hashNum <= 9 - row - counter; hashNum++) {
+            cout << "#";
+        }
+
+        cout << "\n";
+
+        counter++;
+    }
+
+    return 0;
+}
+
